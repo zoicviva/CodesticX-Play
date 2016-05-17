32831e48dfbc0ce973a527d0af8e813cb435355c
vivek keshri <vivekcheti@gmail.com>
2016-05-17 12:24:39 +0530

    hihihihi
    joojojo

--- StartingPoint.py
+++ StartingPoint.py
@@ -19,8 +19,8 @@ import time
 
 
 top = Tkinter.Tk()
-checkVar1 = IntVar()
-checkVar2 = IntVar()
+# checkVar1 = IntVar()
+# checkVar2 = IntVar()
 file1=""
 file2=""
 userHome=os.path.expanduser('~')
@@ -38,16 +38,88 @@ def openNavigateHtml():
     resultBoxGlob.destroy()
     return "Success"
 
-def startButtonCallBack():
+# def startButtonCallBack():
+#     
+#     if checkVar1.get()==0 and checkVar2.get()==0 :
+#         tkMessageBox.showinfo( "Code Compliance", "Atleast select one check box")
+#     else :
+#         filePannel=Tk() # we don't want a full GUI, so keep the root window from appearing
+#         filePannel.withdraw()
+#         fileNameSelected=askopenfilename() # show an "Open" dialog box and return the path to the selected file
+#         filePannel.destroy()
+#         
+#         fileName=UtilitiesCC.copyFileToOrig(fileNameSelected)
+#         logging.info(fileName+" copied to orig folder")
+#         fileContent=CommentHandler(fileName).removeComments()
+#         UtilitiesCC.writeTextToFile(userHome+"/CodeCompliance/work", fileName, fileContent)
+#         fileObj=StatementSequencer(fileName)
+#         sequencedfilePath=fileObj.sequenceIt()
+#         analyse=Analyser(fileName).startAnalysing(fileName)
+#         msg=""
+#         if checkVar1.get()==1:
+#             global file1
+#             file1=OperationCount(fileName).tableWiseCount()
+#             msg+="Statement Counts : "+file1+"\n"
+#         else:
+#             file1=""
+#             print "not calling table wise count"
+#         if checkVar2.get()==1:
+#             global file2
+#             file2=TableFlow(fileName).tableFlowGenerator()
+#             msg+="Table flow : "+file2+"\n"
+#         else:
+#             file2=""
+#             print "not calling table flow"
+#         
+#         resultBox=Tkinter.Tk()
+#         global resultBoxGlob
+#         resultBoxGlob=resultBox
+#         resultBox.wm_title("Code Compliance - Success!")
+#         w = 590 # width for the Tk root
+#         h = 220 # height for the Tk root
+#         ws = top.winfo_screenwidth() # width of the screen
+#         hs = top.winfo_screenheight() # height of the screen
+#         x = (ws/2) - (w/2)
+#         y = (hs/2) - (h/2)
+#         resultBox.geometry('%dx%d+%d+%d' % (w, h, x, y))
+#         resultBox.lift()
+#         text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
+#         text.pack()
+#         openButton = Tkinter.Button(resultBox, text ="Open", command = openHtmlInBrowser)
+#         openButton.pack()
     
-    if checkVar1.get()==0 and checkVar2.get()==0 :
-        tkMessageBox.showinfo( "Code Compliance", "Atleast select one check box")
-    else :
-        filePannel=Tk() # we don't want a full GUI, so keep the root window from appearing
-        filePannel.withdraw()
-        fileNameSelected=askopenfilename() # show an "Open" dialog box and return the path to the selected file
-        filePannel.destroy()
-        
+def startFolderButtonCallBack():
+    filePannel=Tk() # we don't want a full GUI, so keep the root window from appearing
+    filePannel.withdraw()
+    folderSelected=askdirectory() # show an "Open" dialog box and return the path to the selected file
+    filePannel.destroy()
+    allFiles = []
+    for (dirpath, dirnames, filenames) in walk(folderSelected):
+        allFiles.extend(filenames)
+        break
+    files=[]
+    for file in allFiles:
+        if not re.search(r"^\.", file):
+            files.append(file)
+    
+    resultBox=Tkinter.Tk()
+    global resultBoxGlob
+    resultBoxGlob=resultBox
+    resultBox.wm_title("Code Compliance - Success!")
+    resultBox.resizable(width=FALSE, height=TRUE)
+    w = 590 # width for the Tk root
+    h = 220 # height for the Tk root
+    ws = top.winfo_screenwidth() # width of the screen
+    hs = top.winfo_screenheight() # height of the screen
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    resultBox.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    resultBox.lift()
+    
+    currentFile=1
+    totalFiles=len(files)
+    for file in files:
+        fileNameSelected=folderSelected+"/"+file
         fileName=UtilitiesCC.copyFileToOrig(fileNameSelected)
         logging.info(fileName+" copied to orig folder")
         fileContent=CommentHandler(fileName).removeComments()
@@ -55,102 +127,17 @@ def startButtonCallBack():
         fileObj=StatementSequencer(fileName)
         sequencedfilePath=fileObj.sequenceIt()
         analyse=Analyser(fileName).startAnalysing(fileName)
-        msg=""
-        if checkVar1.get()==1:
-            global file1
-            file1=OperationCount(fileName).tableWiseCount()
-            msg+="Statement Counts : "+file1+"\n"
-        else:
-            file1=""
-            print "not calling table wise count"
-        if checkVar2.get()==1:
-            global file2
-            file2=TableFlow(fileName).tableFlowGenerator()
-            msg+="Table flow : "+file2+"\n"
-        else:
-            file2=""
-            print "not calling table flow"
-        
-        resultBox=Tkinter.Tk()
-        global resultBoxGlob
-        resultBoxGlob=resultBox
-        resultBox.wm_title("Code Compliance - Success!")
-        w = 590 # width for the Tk root
-        h = 220 # height for the Tk root
-        ws = top.winfo_screenwidth() # width of the screen
-        hs = top.winfo_screenheight() # height of the screen
-        x = (ws/2) - (w/2)
-        y = (hs/2) - (h/2)
-        resultBox.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        resultBox.lift()
-        text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
-        text.pack()
-        openButton = Tkinter.Button(resultBox, text ="Open", command = openHtmlInBrowser)
-        openButton.pack()
-    
-def startFolderButtonCallBack():
-    
-    if checkVar1.get()==0 and checkVar2.get()==0 :
-        tkMessageBox.showinfo( "Code Compliance", "Atleast select one check box")
-    else :
-        filePannel=Tk() # we don't want a full GUI, so keep the root window from appearing
-        filePannel.withdraw()
-        folderSelected=askdirectory() # show an "Open" dialog box and return the path to the selected file
-        filePannel.destroy()
-        allFiles = []
-        for (dirpath, dirnames, filenames) in walk(folderSelected):
-            allFiles.extend(filenames)
-            break
-        files=[]
-        for file in allFiles:
-            if not re.search(r"^\.", file):
-                files.append(file)
-        
-        resultBox=Tkinter.Tk()
-        global resultBoxGlob
-        resultBoxGlob=resultBox
-        resultBox.wm_title("Code Compliance - Success!")
-        resultBox.resizable(width=FALSE, height=TRUE)
-        w = 590 # width for the Tk root
-        h = 220 # height for the Tk root
-        ws = top.winfo_screenwidth() # width of the screen
-        hs = top.winfo_screenheight() # height of the screen
-        x = (ws/2) - (w/2)
-        y = (hs/2) - (h/2)
-        resultBox.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        resultBox.lift()
-        
-        currentFile=1
-        totalFiles=len(files)
-#         msg="analysis in process!!"
-#         text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
-#         text.pack()
-        for file in files:
-#             msg="("+str(currentFile)+"/"+str(totalFiles)+") Processing file "+file
-#             text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
-#             text.pack()
-            fileNameSelected=folderSelected+"/"+file
-            fileName=UtilitiesCC.copyFileToOrig(fileNameSelected)
-            logging.info(fileName+" copied to orig folder")
-            fileContent=CommentHandler(fileName).removeComments()
-            UtilitiesCC.writeTextToFile(userHome+"/CodeCompliance/work", fileName, fileContent)
-            fileObj=StatementSequencer(fileName)
-            sequencedfilePath=fileObj.sequenceIt()
-            analyse=Analyser(fileName).startAnalysing(fileName)
-            if checkVar1.get()==1:
-                OperationCount(fileName).tableWiseCount()
-            if checkVar2.get()==1:
-                TableFlow(fileName).tableFlowGenerator()
-            currentFile+=1        
-#         tkMessageBox.showinfo( "Code Compliance", "Processing completed!")
-        ComplexityChart().csvFile(files) 
-        ApplicationContainer().buildContainer(files)
-        msg="Navigation Page :"+userHome+"/CodeCompliance/html/index.html"
-        text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
-        text.pack()
-        openButton = Tkinter.Button(resultBox, text ="Open", command = openNavigateHtml)
-        openButton.pack()
-        top.destroy()
+        OperationCount(fileName).tableWiseCount()
+        TableFlow(fileName).tableFlowGenerator()
+        currentFile+=1        
+    ComplexityChart().csvFile(files) 
+    ApplicationContainer().buildContainer(files)
+    msg="Navigation Page :"+userHome+"/CodeCompliance/html/index.html"
+    text=Label(resultBox,bd=0,padx=50,pady=50,text=msg,wraplength=590 )
+    text.pack()
+    openButton = Tkinter.Button(resultBox, text ="Open", command = openNavigateHtml)
+    openButton.pack()
+    top.destroy()
 
 # def quitMainProgram():
 #     top.destroy()
@@ -165,7 +152,7 @@ if __name__=="__main__" :
     top.update_idletasks()
     top.resizable(width=FALSE, height=FALSE)
     w = 390 # width for the Tk root
-    h = 170 # height for the Tk root
+    h = 150 # height for the Tk root
     ws = top.winfo_screenwidth() # width of the screen
     hs = top.winfo_screenheight() # height of the screen
     x = (ws/2) - (w/2)
@@ -179,12 +166,12 @@ if __name__=="__main__" :
 #     fileButton = Tkinter.Button(top, text ="Select File", command = startButtonCallBack)
 #     fileButton.pack({"side": "right"})
     folderButton = Tkinter.Button(top, text ="Select Folder", command = startFolderButtonCallBack)
-    folderButton.pack({"side": "right"})
+    folderButton.pack()
 #     quitBttn = Tkinter.Button(top, text ="Quit", command = quitMainProgram)
 #     quitBttn.pack({"side": "right"})
-    c1 = Checkbutton(top, text = "Statement Count", variable = checkVar1,onvalue = 1, offvalue = 0)
-    c2 = Checkbutton(top, text = "Table Flow", variable = checkVar2,onvalue = 1, offvalue = 0)
-    c1.pack()
-    c2.pack()
+#     c1 = Checkbutton(top, text = "Statement Count", variable = checkVar1,onvalue = 1, offvalue = 0)
+#     c2 = Checkbutton(top, text = "Table Flow", variable = checkVar2,onvalue = 1, offvalue = 0)
+#     c1.pack()
+#     c2.pack()
     top.mainloop()
     
